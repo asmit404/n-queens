ad50c82ec4f16c760a22a562575fb4b5e56000c5
Asmit Singh <asmittbhardwaj@gmail.com>
2024-10-13 13:00:25 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,131 +1,157 @@
-'use strict';
-
+'use strict'
 const numberbox = document.getElementById("numberbox");
 const slider = document.getElementById("slider");
-const progressBar = document.getElementById("progress-bar");
+const progressBar = document.getElementById("progress-bar")
 const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById("pause-button");
 
 const queen = '<i class="fas fa-chess-queen" style="color:#000"></i>';
 
 let n, speed, tempSpeed, q, Board = 0;
-const array = [0, 2, 1, 1, 3, 11, 5, 41, 93];
-let pos = {};
+// Board = 0;
 
+// Creating array for all the possible arrangements of the N-Queen
+let array = [0, 2, 1, 1, 3, 11, 5, 41, 93];
+
+// Used to store the state of the boards;
+let pos = {};
+// let position = {};
+
+
+// Setting the slider value onSlide
 speed = (100 - slider.value) * 10;
 tempSpeed = speed;
-
-slider.oninput = debounce(function () {
+slider.oninput = function () {
     progressBar.style.width = this.value + "%";
-    speed = (100 - slider.value) * 10;
-}, 100);
+    speed = slider.value;
+    speed = (100 - speed) * 10;
+}
 
 class Queen {
     constructor() {
-        this.position = { ...pos };
+        this.position = Object.assign({}, pos);
+        // this.Board = 0;
         this.uuid = [];
     }
 
-    async nQueen() {
+    nQueen = async () => {
         Board = 0;
         this.position[`${Board}`] = {};
         numberbox.disabled = true;
-        await this.solveQueen(Board, 0, n);
-        await this.clearColor(Board);
+        await q.solveQueen(Board, 0, n);
+        await q.clearColor(Board);
         numberbox.disabled = false;
     }
 
-    async isValid(board, r, col, n) {
+    isValid = async (board, r, col, n) => {
+        //Setting the current box color to orange
         const table = document.getElementById(`table-${this.uuid[board]}`);
         const currentRow = table.firstChild.childNodes[r];
         const currentColumn = currentRow.getElementsByTagName("td")[col];
         currentColumn.innerHTML = queen;
-        await this.delay();
+        // currentColumn.style.backgroundColor = "#FF9F1C";
+        await q.delay();
 
+        // Checking the queen in the same column
         for (let i = r - 1; i >= 0; --i) {
             const row = table.firstChild.childNodes[i];
             const column = row.getElementsByTagName("td")[col];
-            if (column.innerHTML == queen) {
+
+            const value = column.innerHTML;
+
+            if (value == queen) {
                 column.style.backgroundColor = "#FB5607";
-                currentColumn.innerHTML = "-";
+                currentColumn.innerHTML = "-"
                 return false;
             }
             column.style.backgroundColor = "#ffca3a";
-            await this.delay();
+            await q.delay();
         }
 
+        //Checking the upper left diagonal
         for (let i = r - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
             const row = table.firstChild.childNodes[i];
             const column = row.getElementsByTagName("td")[j];
-            if (column.innerHTML == queen) {
+            const value = column.innerHTML;
+
+            if (value == queen) {
                 column.style.backgroundColor = "#fb5607";
-                currentColumn.innerHTML = "-";
+                currentColumn.innerHTML = "-"
                 return false;
             }
             column.style.backgroundColor = "#ffca3a";
-            await this.delay();
+            await q.delay();
         }
 
+        // Checking the upper right diagonal
         for (let i = r - 1, j = col + 1; i >= 0 && j < n; --i, ++j) {
             const row = table.firstChild.childNodes[i];
             const column = row.getElementsByTagName("td")[j];
-            if (column.innerHTML == queen) {
+
+            const value = column.innerHTML;
+
+            if (value == queen) {
                 column.style.backgroundColor = "#FB5607";
-                currentColumn.innerHTML = "-";
+                currentColumn.innerHTML = "-"
                 return false;
             }
             column.style.backgroundColor = "#ffca3a";
-            await this.delay();
+            await q.delay();
         }
         return true;
     }
 
-    async clearColor(board) {
-        const table = document.getElementById(`table-${this.uuid[board]}`);
+    clearColor = async (board) => {
         for (let j = 0; j < n; ++j) {
+            const table = document.getElementById(`table-${this.uuid[board]}`);
             const row = table.firstChild.childNodes[j];
-            for (let k = 0; k < n; ++k) {
-                row.getElementsByTagName("td")[k].style.backgroundColor = (j + k) & 1 ? "#FF9F1C" : "#FCCD90";
-            }
+            for (let k = 0; k < n; ++k)
+                (j + k) & 1
+                    ? (row.getElementsByTagName("td")[k].style.backgroundColor = "#FF9F1C")
+                    : (row.getElementsByTagName("td")[k].style.backgroundColor = "#FCCD90");
         }
     }
 
-    async delay() {
-        await new Promise((done) => setTimeout(done, speed));
+    delay = async () => {
+        await new Promise((done) => setTimeout(() => done(), speed));
     }
 
-    async solveQueen(board, r, n) {
+    solveQueen = async (board, r, n) => {
         if (r == n) {
             ++Board;
-            const table = document.getElementById(`table-${this.uuid[Board]}`);
+            let table = document.getElementById(`table-${this.uuid[Board]}`);
             for (let k = 0; k < n; ++k) {
-                const row = table.firstChild.childNodes[k];
+                let row = table.firstChild.childNodes[k];
                 row.getElementsByTagName("td")[this.position[board][k]].innerHTML = queen;
             }
-            this.position[Board] = { ...this.position[board] };
+            this.position[Board] = this.position[board];
             return;
         }
 
         for (let i = 0; i < n; ++i) {
-            await this.delay();
-            await this.clearColor(board);
-            if (await this.isValid(board, r, i, n)) {
-                await this.delay();
-                await this.clearColor(board);
-                const table = document.getElementById(`table-${this.uuid[board]}`);
-                const row = table.firstChild.childNodes[r];
+            await q.delay();
+            // console.log("outside:" + board);
+            await q.clearColor(board);
+            if (await q.isValid(board, r, i, n)) {
+                await q.delay();
+                // console.log("inside:" + board)
+                await q.clearColor(board);
+                let table = document.getElementById(`table-${this.uuid[board]}`);
+                let row = table.firstChild.childNodes[r];
                 row.getElementsByTagName("td")[i].innerHTML = queen;
 
                 this.position[board][r] = i;
 
-                if (await this.solveQueen(board, r + 1, n)) {
-                    await this.clearColor(board);
-                }
+                if (await q.solveQueen(board, r + 1, n))
+                    await q.clearColor(board);
 
-                await this.delay();
+                await q.delay();
                 board = Board;
-                const newTable = document.getElementById(`table-${this.uuid[board]}`);
-                const newRow = newTable.firstChild.childNodes[r];
-                newRow.getElementsByTagName("td")[i].innerHTML = "-";
+                // console.log(this.Board)
+                table = document.getElementById(`table-${this.uuid[board]}`);
+                // console.log(JSON.parse(JSON.stringify(table)));
+                row = table.firstChild.childNodes[r];
+                row.getElementsByTagName("td")[i].innerHTML = "-";
 
                 delete this.position[`${board}`][`${r}`];
             }
@@ -140,17 +166,22 @@ playButton.onclick = async function visualise() {
     n = numberbox.value;
     q = new Queen();
 
-    if (n > 8 || n < 1) {
+    if (n > 8) {
         numberbox.value = "";
-        alert("Queen value is out of range");
+        alert("Queen value is too large");
+        return;
+    } else if (n < 1) {
+        numberbox.value = "";
+        alert("Queen value is too small");
         return;
     }
 
-    while (chessBoard.firstChild) {
+    // Removing all the of previous execution context
+    while (chessBoard.hasChildNodes()) {
         chessBoard.removeChild(chessBoard.firstChild);
     }
-    while (arrangement.firstChild) {
-        arrangement.removeChild(arrangement.firstChild);
+    if (arrangement.hasChildNodes()) {
+        arrangement.removeChild(arrangement.lastChild)
     }
 
     const para = document.createElement("p");
@@ -158,26 +189,33 @@ playButton.onclick = async function visualise() {
     para.innerHTML = `For ${n}x${n} board, ${array[n] - 1} arrangements are possible.`;
     arrangement.appendChild(para);
 
-    for (let i = 0; i < array[n]; ++i) {
-        q.uuid.push(Math.random());
-        const div = document.createElement('div');
-        const table = document.createElement('table');
-        const header = document.createElement('h4');
-        header.innerHTML = `Board ${i + 1}`;
-        table.setAttribute("id", `table-${q.uuid[i]}`);
-        header.setAttribute("id", `paragraph-${i}`);
-        chessBoard.appendChild(div);
-        div.appendChild(header);
-        div.appendChild(table);
+    //Adding boards to the Div
+    if (chessBoard.childElementCount === 0) {
+        for (let i = 0; i < array[n]; ++i) {
+            q.uuid.push(Math.random());
+            let div = document.createElement('div');
+            let table = document.createElement('table');
+            let header = document.createElement('h4');
+            // div.setAttribute("id", `div-${100 + uuid[i]}`)
+            header.innerHTML = `Board ${i + 1} `
+            table.setAttribute("id", `table-${q.uuid[i]}`);
+            header.setAttribute("id", `paragraph-${i}`);
+            chessBoard.appendChild(div);
+            div.appendChild(header);
+            div.appendChild(table);
+        }
     }
 
     for (let k = 0; k < array[n]; ++k) {
-        const table = document.getElementById(`table-${q.uuid[k]}`);
+        let table = document.getElementById(`table-${q.uuid[k]}`);
         for (let i = 0; i < n; ++i) {
-            const row = table.insertRow(i);
+            const row = table.insertRow(i); // inserting ith row
+            row.setAttribute("id", `Row${i} `);
             for (let j = 0; j < n; ++j) {
-                const col = row.insertCell(j);
-                col.style.backgroundColor = (i + j) & 1 ? "#FF9F1C" : "#FCCD90";
+                const col = row.insertCell(j); // inserting jth column
+                (i + j) & 1
+                    ? (col.style.backgroundColor = "#FF9F1C")
+                    : (col.style.backgroundColor = "#FCCD90");
                 col.innerHTML = "-";
                 col.style.border = "0.3px solid #373f51";
             }
@@ -186,11 +224,3 @@ playButton.onclick = async function visualise() {
     }
     await q.nQueen();
 };
-
-function debounce(func, wait) {
-    let timeout;
-    return function (...args) {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(this, args), wait);
-    };
-}
